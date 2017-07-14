dad839e6a2bbe6d88ca9ed058f9e29f0d49a31d5
Mike Smith <mikejamesjsmith@gmail.com>
2017-07-14 15:33:45 +0100

    Added 3d CNN for infarctions

--- 3d-2channel-fakedata-infarc.py
+++ 3d-2channel-fakedata-infarc.py
@@ -13,7 +13,7 @@ import datetime
 
 # Import and preprocess data
 
-def importHeartData(calmFile, stressFile, resize):
+def importHeartData(calmFile, resize):
     """
     Import heart data and extract the pixel array.
     Slice halfway along ind axis.
@@ -21,22 +21,17 @@ def importHeartData(calmFile, stressFile, resize):
     If resize == 1, interpolate data to fit (34,34) arr.
     """
     calmTmp = pyfits.open(calmFile)[0].data
-    stressTmp = pyfits.open(stressFile)[0].data
 
     calmTmp = cropHeart(calmTmp)
-    stressTmp = cropHeart(stressTmp)
 
     # Pad the 3d arrays with zeros so that they are all the same size
     zeroArr0 = np.zeros((34,34,34))
-    zeroArr1 = np.zeros((34,34,34))
 
     if resize == 1:
         # Resize the 2D slices
         calmRatio = 34.0/np.amax(calmTmp.shape)
-        stressRatio = 34.0/np.amax(stressTmp.shape)
 
         calm3d = scipy.ndimage.interpolation.zoom(calmTmp, (calmRatio))
-        stress3d = scipy.ndimage.interpolation.zoom(stressTmp, (stressRatio))
 
         if calm3d.shape[0] != 34:
             startInd = (34 - calm3d.shape[0])/2
@@ -48,27 +43,13 @@ def importHeartData(calmFile, stressFile, resize):
             startInd = (34 - calm3d.shape[2])/2
             zeroArr0[:calm3d.shape[0],:calm3d.shape[1],startInd:calm3d.shape[2]+startInd] = calm3d
 
-
-        if stress3d.shape[0] != 34:
-            startInd = (34 - stress3d.shape[0])/2
-            zeroArr1[startInd:stress3d.shape[0]+startInd,:stress3d.shape[1],:stress3d.shape[2]] = stress3d
-        if stress3d.shape[1] != 34:
-            startInd = (34 - stress3d.shape[1])/2
-            zeroArr1[:stress3d.shape[0],startInd:stress3d.shape[1]+startInd,:stress3d.shape[2]] = stress3d
-        if stress3d.shape[2] != 34:
-            startInd = (34 - stress3d.shape[2])/2
-            zeroArr1[:stress3d.shape[0],:stress3d.shape[1],startInd:stress3d.shape[2]+startInd] = stress3d
-
     else:
         zeroArr0[:calm3d.shape[0],:calm3d.shape[1],:calm3d.shape[2]] = calm3d
-        zeroArr1[:stress3d.shape[0],:stress3d.shape[1],stress3d.shape[2]] = stress3d
 
     for i in np.arange(zeroArr0.shape[0]):
         zeroArr0[i] = sklearn.preprocessing.normalize(zeroArr0[i])
-        zeroArr1[i] = sklearn.preprocessing.normalize(zeroArr1[i])
 
-    catOut = [zeroArr0, zeroArr1]
-    return catOut
+    return zeroArr0
 
 def importType(pptType, n):
     """
@@ -78,10 +59,9 @@ def importType(pptType, n):
     tmplst = []
     simsDir = "/data/jim/Heart/sims/"
     for i in np.arange(0,n):
-        cwdStress = str(simsDir+"stress_"+pptType+"_%0.4d.fits") %i
         cwdCalm = str(simsDir+"rest_"+pptType+"_%0.4d.fits") %i
         # Get zoomed 3d arrays:
-        xAx = importHeartData(cwdCalm, cwdStress, 1) # zoom = 1
+        xAx = importHeartData(cwdCalm, 1) # zoom = 1
         tmplst.append(xAx)
 
     dataFile = np.array(tmplst)
@@ -108,13 +88,11 @@ def cropHeart(inp):
 if __name__ == "__main__":
 
     # Do data import
-    abName = "ischaemia"
+    abName = "infarction"
     abDat = importType(abName,500)
-    abDat = np.moveaxis(abDat,1,-1)
 
     normName = "healthy"
     normDat = importType(normName,500) # Normal and abnormal data same number of ppts
-    normDat = np.moveaxis(normDat,1,-1)
 
     inData = np.concatenate([normDat, abDat])
 
@@ -125,7 +103,7 @@ if __name__ == "__main__":
 
     # Mutual shuffle
     shufData, shufLab = sklearn.utils.shuffle(inData, labels, random_state=1)
-    shufData = np.reshape(shufData,(-1,34,34,34,2))
+    shufData = np.reshape(shufData,(-1,34,34,34,1))
     shufLabOH = np.eye(2)[shufLab.astype(int)] # One hot encode
 
     # k fold the data
@@ -146,7 +124,7 @@ if __name__ == "__main__":
         tflearn.initializations.normal()
 
         # Input layer:
-        net = tflearn.layers.core.input_data(shape=[None,34,34,34,2])
+        net = tflearn.layers.core.input_data(shape=[None,34,34,34,1])
 
         # First layer:
         net = tflearn.layers.conv.conv_3d(net, 32, [10,10,10],  activation="leaky_relu")
