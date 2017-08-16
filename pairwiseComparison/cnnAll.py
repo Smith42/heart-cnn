da12e5d3946d204b7627790b943f19dd1f7adf9b
Mike Smith <mikejamesjsmith@gmail.com>
2017-08-16 10:55:57 +0100

    cnnAll should now have accuracies for each type of cube, added some regularisation too

--- pairwiseComparison/cnnAll.py
+++ pairwiseComparison/cnnAll.py
@@ -42,29 +42,37 @@ if __name__ == "__main__":
     net = tflearn.layers.conv.conv_3d(net, 128, [2,2,2], activation="leaky_relu") # This was added for CNN 2017-07-28
 
     # Fully connected layers
-    net = tflearn.layers.core.fully_connected(net, 2048, activation="leaky_relu") # regularizer="L2", weight_decay=0.01,
+    net = tflearn.layers.core.fully_connected(net, 2048, activation="leaky_relu", regularizer="L2", weight_decay=0.01)
     #net = tflearn.layers.core.dropout(net, keep_prob=0.5)
 
-    net = tflearn.layers.core.fully_connected(net, 1024, activation="leaky_relu") # regularizer="L2", weight_decay=0.01,
+    net = tflearn.layers.core.fully_connected(net, 1024, activation="leaky_relu", regularizer="L2", weight_decay=0.01)
     #net = tflearn.layers.core.dropout(net, keep_prob=0.5)
 
-    net = tflearn.layers.core.fully_connected(net, 512, activation="leaky_relu") # regularizer="L2", weight_decay=0.01,
+    net = tflearn.layers.core.fully_connected(net, 512, activation="leaky_relu", regularizer="L2", weight_decay=0.01)
     #net = tflearn.layers.core.dropout(net, keep_prob=0.5)
 
     # Output layer:
-    net = tflearn.layers.core.fully_connected(net, 4, activation="softmax")
+    net = tflearn.layers.core.fully_connected(net, 5, activation="softmax")
 
     net = tflearn.layers.estimator.regression(net, optimizer='adam', learning_rate=0.0001, loss='categorical_crossentropy')
     model = tflearn.DNN(net, tensorboard_verbose=0)
 
-    # Train the model, leaving out the kfold not being used
-    model.fit(inData, inLabelsOH, batch_size=100, n_epoch=60, show_metric=True, validation_set=0.1) # Need validation so I can see when learning stops
+    # Train the model
+    model.fit(inData, inLabelsOH, batch_size=100, n_epoch=30, show_metric=True, validation_set=0.1) # Need validation so I can see when learning stops
     dt = str(datetime.datetime.now().replace(second=0, microsecond=0).isoformat("_"))
     model.save("./models/"+dt+"_3d-2channel-fakedata-all.tflearn")
+
+    # Retrieve indices of the different cubes and get the accuracies of each type
+    inLabels_test = np.argwhere(inLabelsOH_test == 1)
+    accNorm = model.evaluate([inData_test[x] for x in np.argwhere(inLabels_test[:,1]==0)], [inLabelsOH_test[x] for x in np.argwhere(inLabels_test[:,1]==0)], batch_size=100)
+    accIs = model.evaluate([inData_test[x] for x in np.argwhere(inLabels_test[:,1]==1)], [inLabelsOH_test[x] for x in np.argwhere(inLabels_test[:,1]==1)], batch_size=100)
+    accIn = model.evaluate([inData_test[x] for x in np.argwhere(inLabels_test[:,1]==2)], [inLabelsOH_test[x] for x in np.argwhere(inLabels_test[:,1]==2)], batch_size=100)
+    accMi = model.evaluate([inData_test[x] for x in np.argwhere(inLabels_test[:,1]==3)], [inLabelsOH_test[x] for x in np.argwhere(inLabels_test[:,1]==3)], batch_size=100)
+    accAr = model.evaluate([inData_test[x] for x in np.argwhere(inLabels_test[:,1]==4)], [inLabelsOH_test[x] for x in np.argwhere(inLabels_test[:,1]==4)], batch_size=100)
 
     acc = model.evaluate(inData_test, inLabelsOH_test, batch_size=100)
 
     savefileacc = "./logs/"+dt+"_3d-2channel-fakedata-acc_all.log"
-    np.savetxt(savefileacc, acc[0], delimiter=",")
+    np.savetxt(savefileacc, (accNorm,accIs,accIn,accMi,acAr,acc), delimiter=",")
     h5f.close()
     h5f_test.close()
