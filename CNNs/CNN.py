dc91ec7f9d553ae43894e91766ef28e1c8152be6
Mike Smith <mikejamesjsmith@gmail.com>
2018-05-11 15:03:50 +0100

    New global pooled CNN.py is ready for testing

--- CNNs/CNN.py
+++ CNNs/CNN.py
@@ -27,24 +27,16 @@ def getCNN(classes, finetune=False):
         conv_2 = tflearn.layers.conv.conv_3d(max_1, 128, [2,2,2], activation="leaky_relu", trainable=False) # This was added for CNN 2017-07-28
         max_2 = tflearn.layers.conv.max_pool_3d(conv_2, [2,2,2], strides=[2,2,2]) # This was added for CNN 2017-08-24
 
-        #Fourth layer:
+        # Fourth layer:
         conv_3 = tflearn.layers.conv.conv_3d(max_2, 256, [2,2,2], activation="leaky_relu", trainable=False) # This was added for CNN 2017-08-24
-        #net = tflearn.layers.core.fully_connected(net, 4096, activation="leaky_relu", trainable=False)#, regularizer="L2", weight_decay=0.0001) # This was added for CNN 2017-08-24
 
-        # Fully connected layers
-        fc_0 = tflearn.layers.core.fully_connected(conv_3, 2048, activation="leaky_relu", regularizer="L2", weight_decay=0.0001, trainable=False)
-        #net = tflearn.layers.core.dropout(net, keep_prob=0.5)
-
-        fc_1 = tflearn.layers.core.fully_connected(fc_0, 1024, activation="leaky_relu", regularizer="L2", weight_decay=0.0001, trainable=False)
-        #net = tflearn.layers.core.dropout(net, keep_prob=0.5)
-
-        fc_2 = tflearn.layers.core.fully_connected(fc_1, 512, activation="leaky_relu", regularizer="L2", weight_decay=0.0001)
-        #net = tflearn.layers.core.dropout(net, keep_prob=0.5)
+        # Global pooling layer:
+        global_pool_0 = tf.reduce_mean(conv_3, [1,2,3])
 
         # Output layer:
-        fc_3 = tflearn.layers.core.fully_connected(fc_2, classes, activation="softmax")
+        fc_0 = tflearn.layers.core.fully_connected(global_pool_0, classes, activation="softmax")
 
-        outp = tflearn.layers.estimator.regression(fc_3, optimizer='adam', learning_rate=0.0001, loss='categorical_crossentropy')
+        outp = tflearn.layers.estimator.regression(fc_0, optimizer='adam', learning_rate=0.0001, loss='categorical_crossentropy')
         model = tflearn.DNN(outp, tensorboard_verbose=0)
 
         return model
@@ -65,24 +57,16 @@ def getCNN(classes, finetune=False):
         conv_2 = tflearn.layers.conv.conv_3d(max_1, 128, [2,2,2], activation="leaky_relu") # This was added for CNN 2017-07-28
         max_2 = tflearn.layers.conv.max_pool_3d(conv_2, [2,2,2], strides=[2,2,2]) # This was added for CNN 2017-08-24
 
-        #Fourth layer:
+        # Fourth layer:
         conv_3 = tflearn.layers.conv.conv_3d(max_2, 256, [2,2,2], activation="leaky_relu") # This was added for CNN 2017-08-24
-        #net = tflearn.layers.core.fully_connected(net, 4096, activation="leaky_relu")#, regularizer="L2", weight_decay=0.0001) # This was added for CNN 2017-08-24
 
-        # Fully connected layers
-        fc_0 = tflearn.layers.core.fully_connected(conv_3, 2048, activation="leaky_relu", regularizer="L2", weight_decay=0.0001)
-        #net = tflearn.layers.core.dropout(net, keep_prob=0.5)
-
-        fc_1 = tflearn.layers.core.fully_connected(fc_0, 1024, activation="leaky_relu", regularizer="L2", weight_decay=0.0001)
-        #net = tflearn.layers.core.dropout(net, keep_prob=0.5)
-
-        fc_2 = tflearn.layers.core.fully_connected(fc_1, 512, activation="leaky_relu", regularizer="L2", weight_decay=0.0001)
-        #net = tflearn.layers.core.dropout(net, keep_prob=0.5)
+        # Global pooling layer:
+        global_pool_0 = tf.reduce_mean(conv_3, [1,2,3])
 
         # Output layer:
-        fc_3 = tflearn.layers.core.fully_connected(fc_2, classes, activation="softmax")
+        fc_0 = tflearn.layers.core.fully_connected(global_pool_0, classes, activation="softmax")
 
-        outp = tflearn.layers.estimator.regression(fc_3, optimizer='adam', learning_rate=0.0001, loss='categorical_crossentropy')
+        outp = tflearn.layers.estimator.regression(fc_0, optimizer='adam', learning_rate=0.0001, loss='categorical_crossentropy')
         model = tflearn.DNN(outp, tensorboard_verbose=0)
 
         return model
